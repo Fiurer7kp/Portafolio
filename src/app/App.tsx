3d510ef638a5d196911f9e6ec4d8da66eca5ff02
Fiurer7kp <jhonjhoncoral@hotmail.com>
2026-04-15 08:09:23 -0500

    feat: wrap app with PreferencesProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter } from 'react-router-dom'
 import Layout from './components/Layout'
 import AppRoutes from './routes'
 import { TransitionProvider } from './context/TransitionContext'
+import { PreferencesProvider } from './context/PreferencesContext'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <TransitionProvider>
-        <Layout>
-          <AppRoutes />
-        </Layout>
-      </TransitionProvider>
+      <PreferencesProvider>
+        <TransitionProvider>
+          <Layout>
+            <AppRoutes />
+          </Layout>
+        </TransitionProvider>
+      </PreferencesProvider>
     </BrowserRouter>
   )
 }
